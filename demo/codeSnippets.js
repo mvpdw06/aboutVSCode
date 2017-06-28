2f2f07b045d4ce1fe401dec57b4b65d5ed26652e
Ryan Hsu <mvpdw06@gmail.com>
2017-06-28 11:18:54 +0800

    add demo code snippets.

---
+++ demo/codeSnippets.js
@@ -0,0 +1,27 @@
+// <內建 code snippets>
+// type if here.
+
+// type for here.
+
+// type foreach here.
+
+
+
+// <Reactjs code snippets>
+// type rcc herer.
+
+// type rccp here.
+
+// type rcjc here.
+
+
+
+// <JavaScript(ES6) code snippets>
+// type imp / imn / imd here.
+
+// type con here.
+
+// type anfn / nfn herer.
+
+// type dob here
+
